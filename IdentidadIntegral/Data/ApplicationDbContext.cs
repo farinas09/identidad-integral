53bae6e9466e5c63cb3252e02484c876cffff8af
Erick Fariñas <efarinas99@gmail.com>
2024-09-10 23:47:12 -0600

    add dbcontext

---
+++ IdentidadIntegral/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using IdentidadIntegral.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentidadIntegral.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<Module> Modules { get; set; }
+        public DbSet<Models.Action> Actions { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+    }
+
+}
