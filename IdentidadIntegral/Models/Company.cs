0ef71b57658fab144fbaea8e8ff19117f657cca7
Erick Fariñas <efarinas99@gmail.com>
2024-09-25 01:15:07 -0600

    create companies table

--- IdentidadIntegral/Models/Company.cs
+++ IdentidadIntegral/Models/Company.cs
@@ -6,10 +6,15 @@ namespace IdentidadIntegral.Models
     {
         [Key]
         public int IdCompany { get; set; }
+        [Display(Name = "Nombre")]
         public string Name { get; set; }
+        [Display(Name = "Descripción")]
         public string Description { get; set; }
+        [Display(Name = "Fecha de Creación")]
         public DateTime CreatedAt { get; set; }
+        [Display(Name = "Fecha de Modificación")]
         public DateTime UpdatedAt { get; set; }
+        [Display(Name = "Habilitado")]
         public bool Enabled { get; set; }
         public ICollection<Project>? Projects { get; set; }
         public ICollection<User>? Users { get; set; }
